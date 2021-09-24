c0293e51ac0b02729fc28b40f5c45cd70ba19f93
Anusha Tuladhar <atuladhar@ucdavis.edu>
2021-09-24 15:56:50 -0700

    added static pages

--- frontend/at_bnb.jsx
+++ frontend/at_bnb.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
  const root = document.getElementById("root");
- ReactDOM.render(<h1>My super awesome app is on it's way...</h1>);
+ ReactDOM.render(<h1>My super awesome app is on it's way...</h1>, root)
 });
 
 
