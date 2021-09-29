3a0aeb271f3bd3b53fa25d93e509948e1776d795
Anusha Tuladhar <atuladhar@ucdavis.edu>
2021-09-29 11:23:01 -0700

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.delete_all
 
 User.create!({username:"demoUser", email: "demo@user.com", password:"demoUser", first_name: 'demo', last_name: 'user'})
 
