e2c6bad8673c74922c18f4f5cb0d3a985547eaf4
Anusha Tuladhar <atuladhar@ucdavis.edu>
2021-10-26 23:31:46 -0700

    create reservation working, still need to work on listingId

--- frontend/components/reservations/create_reservation_container.jsx
+++ frontend/components/reservations/create_reservation_container.jsx
@@ -2,35 +2,43 @@ import { connect } from "react-redux";
 import ReservationForm from "../reservations/reservation_form";
 import {
   createReservation,
+  fetchReservations,
+  fetchReservation,
+  deleteReservation,
   clearErrors,
 } from "../../actions/reservation_actions"; 
 import{
   fetchListings, fetchListing
 } from "../../actions/listing_actions";
+import{openModal} from "../../actions/modal_actions";
+import{login} from "../../actions/session_actions";
 
 
 const mSTP = (state, ownProps) => {
-  console.log(state);
-  console.log(state.session.id)
-  console.log("i am here the state");
+  console.log("i am here", state.entities.listings[ownProps.listingId]);
   return {
     reservation: {
       check_in_date: "",
       check_out_date: "",
       number_of_guest: "",
       guest_id: state.session.id,
-      listing_id: state.entities.listings[ownProps.match.params.listingId],
+      listing_id: state.session.id
     },
     reservationFormType: "Create Reservation",
-    errors: state.errors.reservation
+    currentUser: state.session.id,
+    errors: state.errors.reservation,
   };
 };
 
 const mDTP = (dispatch) => {
   return {
-    action: (reservation) => dispatch(createReservation(reservation)),
+    createReservation: (reservation) => dispatch(createReservation(reservation)),
     fetchListing: (listingId) => dispatch(fetchListing(listingId)),
+    fetchReservations: () => dispatch(fetchReservations()),
+    fetchReservation: (reservationId) => dispatch(fetchReservation(reservationId)), 
     clearErrors: () => dispatch(clearErrors()),
+    openModal: (e) => dispatch(openModal(e)),
+    login: (user) => dispatch(login(user))
   };
 }
 
--- frontend/components/reservations/reservation_form.jsx
+++ frontend/components/reservations/reservation_form.jsx
@@ -1,46 +1,71 @@
 import React from "react";
 import { withRouter } from "react-router";
 
+
 class ReservationForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = this.props.reservation;
-
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.updateReservation = this.updateReservation.bind(this);
+    this.reservation = this.reservation.bind(this);
+    this.newState = Object.assign({}, this.props.reservation)
+  }
+  componentDidMount() {
+    this.props.fetchListing(this.props.match.params.listingId);
   }
 
-  updateReservation(e, field) {
+  reservation(e, field) {
     this.setState({ [field]: e.target.value });
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    this.props.action(this.state);
+    // this.props.action(this.state);
+    if (this.props.currentUser) {
+      if (this.state.check_out_date < this.state.check_in_date) return null;
+      const reservations = Object.assign({}, this.state, {
+        listing_id: this.props.listingId,
+      });
+      this.props
+        .createReservation(reservations)
+        .then(this.props.history.push("/reservations"));
+      this.setState(this.newState);
+    } else {
+      this.props.openModal("login");
+    }
+    this.props.createReservation(this.state);
   }
 
   render() {
     return (
       <div>
-      {/* <form onSubmit={this.handleSubmit}>
-        <label>
-          CHECK-IN
-          <input
-            type="date"
-            value={this.state.check_in_date}
-            onChange={(e) => this.updateReservation(e, "check_in_date")}
-          />
-        </label>
-        <label>
-          CHECK-OUT
-          <input
-            value={this.state.check_out_date}
-            onChange={(e) => this.updateReservation(e, "check_out_date")}
-          />
-        </label>
-        <button>{this.props.formType}</button>
-      </form> */}
-      Hellooooo!!!!
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            CHECK-IN
+            <input
+              type="date"
+              value={this.state.check_in_date}
+              onChange={(e) => this.reservation(e, "check_in_date")}
+            />
+          </label>
+          <label>
+            CHECK-OUT
+            <input
+              type="date"
+              value={this.state.check_out_date}
+              onChange={(e) => this.reservation(e, "check_out_date")}
+            />
+          </label>
+          <label>
+            Guests
+            <input
+              type="number"
+              value={this.state.number_of_guest}
+              onChange={(e) => this.reservation(e, "number_of_guest")}
+            />
+          </label>
+          <button>Create Reservation</button>
+        </form>
       </div>
     );
   }
